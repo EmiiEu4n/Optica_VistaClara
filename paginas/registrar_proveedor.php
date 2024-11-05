35e7c49db7773867e270860dddfef3296d0eb882
MajitoCan <mariajose.can23@gmail.com>
2024-11-05 00:26:36 -0600

    Validacion_MejorasDePlaceholder

--- paginas/registrar_proveedor.php
+++ paginas/registrar_proveedor.php
@@ -20,27 +20,28 @@
 
             <div class="content-registrar formulario">
                 <form action="../php/create_proveedor.php" method="post">
-                    <label for="">Los campos con <span>*</span> son obligatorios.</label><br>
+                    <label for="">Los campos con <span>*</span> son obligatorios.</label><br><br>
                     <fieldset>
                         <legend>Información de la empresa</legend>
                         <!-- nombre -->
                         <label for="">Nombre<span>*</span></label><br>
-                        <input required name="nombre" type="text" placeholder="Escribe el nombre de la empresa Ej.Miller&Marc"><br><br>
-                        <!-- Dirección -->
+                        <input class="validar-espacios" required name="nombre" type="text" placeholder="Nombre de la empresa (Ej. Miller&Marc)"><br><br>
+                        <!-- direccion -->
                         <label for="">Dirección<span>*</span></label><br>
-                        <input required name="direccion" type="text" placeholder="Escribe la dirección de la empresa"><br><br>
+                        <input class="validar-espacios" pattern="^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s\.\-\#\/\°]+$" required name="direccion" type="text" placeholder="Dirección de la empresa" minlength="5" maxlength="150">
                     </fieldset>
+                    <br>
                     <fieldset>
                         <legend>Información del trabajador de la empresa</legend>
                         <!-- Nombre del trabajador -->
                         <label for="">Nombre<span>*</span></label><br>
-                        <input pattern="[a-zA-Z\s]{3,254}" required name="contacto" type="text" placeholder="Ingresa el nombre del trabajador de la empresa"><br><br>
+                        <input class="validar-espacios" pattern="[a-zA-Z\s]{3,254}" required name="contacto" type="text" placeholder="Nombre del trabajador"><br><br>
                         <!-- telefono del trabajador-->
                         <label for="">Teléfono<span>*</span></label><br>
-                        <input id="telefono" pattern="[0-9]{10}" maxlength="10" required placeholder="Ingresa el numero celular Ejemplo.9999123456" title="Ejemplo: 9999123456" name="telefono" type="tel"><br><br>
+                        <input class="validar-espacios" id="telefono" pattern="[0-9]{10}" maxlength="10" required placeholder="Número de celular (Ej. 9999123456)" title="Ejemplo: 9999123456" name="telefono" type="tel"><br><br>
                         <!-- Correo del trabajador -->
                         <label for="">Correo electronico<span>*</span></label><br>
-                        <input required name="correo" type="email" placeholder="Ingrese el correo electronico del empleado"><br><br>
+                        <input class="validar-espacios" required name="correo" type="email" placeholder="Correo electrónico del trabajador"><br><br>
                     </fieldset>
 
                     <div class="opciones-btn opciones-btn-registrar">
